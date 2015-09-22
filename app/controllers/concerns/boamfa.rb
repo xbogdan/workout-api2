72be422c00be346868d1bf9f4870281fc47a51c9
Bogdan Boamfa <bogdan.boamfa@gmail.com>
2015-09-22 11:29:00 +0300

    a

--- app/controllers/concerns/boamfa.rb
+++ app/controllers/concerns/boamfa.rb
@@ -1,4 +1,4 @@
-module Authenticable extend ActiveSupport::Concern 
+module Boamfa extend ActiveSupport::Concern 
   # Devise methods overwrites
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
