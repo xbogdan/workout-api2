3b3b6ccf48bba3d9c24e9f8be197c9c075501026
Bogdan Boamfa <bogdan.boamfa@gmail.com>
2015-09-22 11:12:21 +0300

    Fix.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
 end
+
+module Authenticable extend ActiveSupport::Concern
+  # Devise methods overwrites
+  def current_user
+    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+  end
+
+  def authenticate_with_token!
+    render json: { status: 'error', errors: "Not authenticated" }, status: :unauthorized unless current_user.present?
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+end
