e5caf086caa3cd33e30084d4af79f919df05868c
Bogdan Boamfa <bogdan.boamfa@gmail.com>
2015-09-22 10:46:57 +0300

    Add concerns to path.

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module WorkoutApi2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths += %W( #{config.root}/app/controllers/concerns )
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.action_dispatch.default_headers = {
